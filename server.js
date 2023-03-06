d0e3139140b1d08f67ae7cccd5fa4dc601df224b
Molly Raik <molly.raik@gmail.com>
2023-03-06 17:54:54 -0500

    wip: adds mongoose morgan and outlines app setup

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-const express = require('express')
+// require dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+// initialize app
+const app = express();
+
+// config settings
+require('dotenv').config();
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// database connection
+mongoose.set('strictQuery', true);
+mongoose.connect(DATABASE_URL);
+
+// database connection error/message
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + 'is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+
+// mount middleware
+app.use(morgan("dev"));
+
+// router
+
+
+// listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Express is listening on port: ${PORT}`));
