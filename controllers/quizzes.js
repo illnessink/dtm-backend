16c0329c3c9393543ef19b1a5b749fd8791e1098
mollyraik <molly.raik@gmail.com>
2023-03-15 19:44:32 -0400

    wip: creates Quiz route

---
+++ controllers/quizzes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Quiz = require('../models/Quiz.js');
+
+// index
+router.get("/", async (req, res) => {
+    try {
+        res.status(200).json(await Quiz.find({}))
+    } catch (error) {
+        res.status(400).json({ message: "something went wrong" });
+    }
+});
+
+
+// create 
+router.post("/:id", async (req, res) => {
+    try {
+      req.body.uid = req.user.uid;
+      const quiz = await Quiz.create(req.body);
+      res.status(201).send({ quiz });
+        console.log(req.body)
+    } catch (error) {
+      res.status(400).json({ message: "something went wrong" });
+    }
+  });
+
+
+module.exports = router;
