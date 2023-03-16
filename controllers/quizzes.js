cc28e48f0cc34e1ebcf5346b44bac8b86b57f952
mollyraik <molly.raik@gmail.com>
2023-03-15 22:02:22 -0400

    wip: fixes quiz index route

--- controllers/quizzes.js
+++ controllers/quizzes.js
@@ -5,8 +5,9 @@ const Quiz = require('../models/Quiz.js');
 // index
 router.get("/", async (req, res) => {
     try {
-        res.status(200).json(await Quiz.find({}))
+        res.status(200).json(await Quiz.find({}));
     } catch (error) {
+        console.log(error) // <--- 
         res.status(400).json({ message: "something went wrong" });
     }
 });
@@ -15,7 +16,7 @@ router.get("/", async (req, res) => {
 // create 
 router.post("/:id", async (req, res) => {
     try {
-      req.body.uid = req.user.uid;
+      req.body.uid = req.params.id;
       const quiz = await Quiz.create(req.body);
       res.status(201).send({ quiz });
         console.log(req.body)
