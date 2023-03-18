cc867efdcc8d0121c459c47dd90e2049f0effbe9
Vanessa Foerster <nessfoerster@gmail.com>
2023-03-17 20:10:11 -0400

    fix: rm escape chars from fb config

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ admin.initializeApp({
       "type": "service_account",
       "project_id": "down-to-match",
       "private_key_id": GOOGLE_PRIVATE_KEY_ID,
-      "private_key": GOOGLE_PRIVATE_KEY.replace(/\n/g, ''),
+      "private_key": GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       "client_email": "firebase-adminsdk-hdtbl@down-to-match.iam.gserviceaccount.com",
       "client_id": GOOGLE_CLIENT_ID,
       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
