157ba26aa79b17107cd1f96f0d694437b7bbccd9
mollyraik <molly.raik@gmail.com>
2023-03-21 04:16:26 -0400

    wip: chat feature working w mongo connection

--- models/Message.js
+++ models/Message.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    body: String,
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
+    message: {
+        type: String,
+        required: true
     },
-    createdAt: Date,
-    updatedAt: Date,
+    chatUsers: {
+        type: Array,
+        required: true,
+    },
+    sender: {
+        type: String,
+        required: true
+    } 
 }, { timestamps: true });
 
 module.exports = mongoose.model("Message", messageSchema);
